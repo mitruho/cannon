0aa83134a69d1ce16e217d8ab29df0b6ade8e401
mitruhaa <charonpersonal@proton.me>
2024-06-12 02:56:30 +0200

    move projectile start position inside the cannon

--- main.py
+++ main.py
@@ -1,15 +1,15 @@
 from kivy.app import App
 from kivy.uix.widget import Widget
-from kivy.uix.boxlayout import BoxLayout
 from kivy.vector import Vector
 from kivy.clock import Clock
 from kivy.core.window import Window
-from kivy.properties import NumericProperty, ObjectProperty
+from kivy.properties import NumericProperty, ObjectProperty, BooleanProperty
 import math
 from cannon_constants import *
 
 class Cannon(Widget):
     angle = NumericProperty(0)
+    cannon_height = NumericProperty(50)
 
     def __init__(self, **kwargs):
         super(Cannon, self).__init__(**kwargs)
@@ -22,6 +22,14 @@ class Cannon(Widget):
         angle = Vector(dx, dy).angle(Vector(1, 0))
         self.angle = max(0, min(90, angle))
 
+    @property
+    def end_x(self):
+        return self.x + self.cannon_height * math.cos(math.radians(self.angle))
+
+    @property
+    def end_y(self):
+        return self.y + self.cannon_height * math.sin(math.radians(self.angle))
+
 class Projectile(Widget):
     projectile_height = BULLET_RADIUS * 2
     projectile_width = BULLET_RADIUS * 2
@@ -31,17 +39,24 @@ class Projectile(Widget):
     time = 0
     launch_speed = BULLET_MAX_VEL
     mass = NumericProperty(1000)  # mass of the projectile in kg
+    visible = BooleanProperty(False)
 
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
         self.launch_angle_radians = 0
         self._clock_event = None
+        self.visible = False
+        self.x = -100  # Start off-screen
+        self.y = -100  # Start off-screen
 
-    def start_moving(self, launch_angle):
+    def start_moving(self, launch_angle, start_x, start_y):
         self.launch_angle_radians = math.radians(launch_angle)
         self.x_velocity = self.launch_speed * math.cos(self.launch_angle_radians)
         self.y_velocity = self.launch_speed * math.sin(self.launch_angle_radians)
         self.time = 0  # reset time
+        self.x = start_x
+        self.y = start_y
+        self.visible = True
         self._clock_event = Clock.schedule_interval(self.move, 1/FPS)  # Update 60 times per second
 
     def stop_moving(self):
@@ -61,8 +76,8 @@ class Projectile(Widget):
             new_y = 0
             self.stop_moving()
 
-        self.x = new_x
-        self.y = new_y
+        self.x += new_x
+        self.y += new_y
 
 class CannonGame(Widget):
     projectile = ObjectProperty(None)
@@ -71,7 +86,7 @@ class CannonGame(Widget):
 
     def on_touch_down(self, touch):
         if self.times_launched == 0:
-            self.projectile.start_moving(self.cannon.angle)
+            self.projectile.start_moving(self.cannon.angle, self.cannon.end_x, self.cannon.end_y)
             self.times_launched += 1
             return super().on_touch_down(touch)
 
